a493ff6d8c9ddbd00182d04765d6abb5bfc5a876
Gunasekhar Matamalam <gunasekhar.matamalam@suse.com>
2024-08-26 13:23:38 -0700

    Added languagelist helper to facilitate dynamic generation of the languages menu
    Signed-off-by: Gunasekhar Matamalam <gunasekhar.matamalam@suse.com>

---
+++ src/helpers/languagelist.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const modules = new Set()
+const extractModuleFromURL = (url) => url.split('/')[3] || null
+
+const languagesList = [
+  { id: 'en', image: 'engFlag.svg', class: 'english', label: 'English' },
+  { id: 'zh', image: 'china.svg', class: 'china', label: '中文' },
+  { id: 'ko', image: 'koFlag.svg', class: 'ko', label: '한국어' },
+  { id: 'ja', image: 'jaFlag.svg', class: 'ja', label: '日本語' },
+]
+
+const getModules = (navigation) => {
+  if (navigation.url) {
+    modules.add(extractModuleFromURL(navigation.url))
+  } else if (Array.isArray(navigation.items)) {
+    navigation.items.forEach((item) => getModules(item))
+  }
+}
+
+module.exports = (navigation, uiRootPath) => {
+  navigation.forEach((nav) => getModules(nav))
+  const res = languagesList.filter((lang) => modules.has(lang.id))
+  const finalResponse = res.map((item) => {
+    return {
+      ...item,
+      uiRootPath,
+    }
+  })
+  return finalResponse
+}
